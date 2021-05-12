577cd0bb57febd608310d6fb8e6b0721896f6c0c
NavaneethAadiraju <84010992+NavaneethAadiraju@users.noreply.github.com>
2021-05-12 20:17:04 +0530

    Add files via upload

---
+++ USERJOB/src/main/java/com/user/job/UserjobApplication.java
@@ -0,0 +1,13 @@
+package com.user.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserjobApplication.class, args);
+	}
+
+}
